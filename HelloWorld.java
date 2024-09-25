a809cc3ea731d27e10503316f347762e31988e9b
Vishnu00S <94123890+Vishnu00S@users.noreply.github.com>
2024-09-25 06:54:38 +0530

    new print line added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hi ... How are you");
         System.out.println("Hi ... I'm Fine");
+        System.out.println("Thank you");
     }
 }
