04cb60d5a5ad44cd17887347a327f42e6643c9a0
Vishnu00S <94123890+Vishnu00S@users.noreply.github.com>
2024-09-25 07:02:44 +0530

    addition pprog added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,10 @@ public class HelloWorld {
         System.out.println("Hi ... How are you");
         System.out.println("Hi ... I'm Fine");
         System.out.println("Thank you");
+         int a = 10;
+        int b= 20;
+        int c = a+ b;
+          System.out.println("Addition of a+b is :"+c);
+        
     }
 }
