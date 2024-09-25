f97cdd82f991819db8f07854a6dc8b7e82c7c635
Vishnu00S <94123890+Vishnu00S@users.noreply.github.com>
2024-09-25 07:06:08 +0530

    Update addition

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
         System.out.println("Hi ... How are you");
         System.out.println("Hi ... I'm Fine");
         System.out.println("Thank you");
-         int a = 10;
+         int a = 100;
         int b= 20;
         int c = a+ b;
           System.out.println("Addition of a+b is :"+c);
