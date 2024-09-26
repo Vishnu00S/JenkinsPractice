8e096cdb2eee03ad9b43811fcf52ffa314d284bc
Vishnu00S <94123890+Vishnu00S@users.noreply.github.com>
2024-09-26 20:59:31 +0530

    new changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,13 +4,8 @@ public class HelloWorld {
         System.out.println("Hi ... I'm Fine");
         System.out.println("Thank you");
      
-        int a = 100;
-        int b= 200;
-        int c = a+ b;
-          System.out.println("Addition of a+b is :"+c);
-          System.out.println("Addition of a+b is :"+c);
-        System.out.println("Hi ... How are you");
-        System.out.println("Hi ... I'm Fine");
+        System.out.println("Hi ... Luffy How are you");
+        System.out.println("Hi ... Zoro I'm Fine");
         
         
     }
